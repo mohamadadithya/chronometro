32fff482d5dc41cf7e15ea5b464832f54f07eba7
Mohamad Adithya <75943603+mohamadadithya@users.noreply.github.com>
2021-08-14 11:28:23 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'SW-001';
+const toCache = [
+    '/',
+    'assets/js/web.webmanifest',
+    'assets/js/register.js',
+    'assets/js/app.js',
+    'assets/css/main.css',
+    'assets/css/all.min.css',
+    'assets/css/animate.min.css',
+    'assets/fonts/Poppins-Light.ttf',
+    'assets/fonts/Poppins-SemiBold.ttf',
+    'assets/webfonts/fa-solid-900.woff',
+    'assets/webfonts/fa-solid-900.woff2',
+    'assets/img/apple-icon.png',
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            return cache.addAll(toCache)
+        })
+        .then(self.skipWaiting())
+    )
+})
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request)
+        .catch(() => {
+            return caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.match(event.request)
+            })
+        })
+    )
+})
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys()
+        .then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+            if (key !== CACHE_NAME) {
+                console.log('[ServiceWorker] delete old cache.', key)
+                return caches.delete(key)
+            }
+            }))
+        })
+        .then(() => self.clients.claim())
+    )
+})
